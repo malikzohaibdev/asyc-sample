4b527f109c2470be9efaa21c8cfdf256f9158530
Zohaib Malik <malikzohiabdev@gmail.com>
2024-05-05 16:04:02 +0500

    return total elapsed time

--- server/main.py
+++ server/main.py
@@ -8,6 +8,9 @@ app = FastAPI()
 
 @app.get("/async")
 async def async_response():
+    start_time = time.time()
+    # sleep for random time between 0 and 2 seconds
     sleep_time = uniform(0.0, 2.0)
     time.sleep(sleep_time)
-    return JSONResponse({"response_time": sleep_time})
+    end_time = time.time()
+    return JSONResponse({"elapsed": end_time - start_time})
