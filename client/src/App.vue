c41e6ee5787141fa3660e0bd5e82fb15811f7655
Zohaib Malik <malikzohiabdev@gmail.com>
2024-05-05 20:02:36 +0500

    add vue project and components

---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue"
+import Sleep from "./components/Sleep.vue"
+const execTime = ref(null)
+const getRequest = async function () {
+    try {
+        const response = await fetch("http://localhost:8000/async")
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+</script>
+<template>
+
+    <Sleep :v-if="execTime" :elapsed-time="execTime"></Sleep>
+
+    <button @click="getRequest">Send Request</button>
+</template>
+
+<style scoped></style>
---
+++ client/src/components/Sleep.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+const props = defineProps(["elapsedTime"])
+
+</script>
+<template>
+    <div>
+        <h1>
+            Elapsed Time
+        </h1>
+        <h3>{{ props.elapsedTime }}</h3>
+    </div>
+</template>
+<style></style>
